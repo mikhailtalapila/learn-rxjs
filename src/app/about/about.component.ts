bd366f7da27a6db1d207b71fb1fa881988f86b4e
Michael Talapila <mikhailtalapila@yahoo.com>
2025-08-04 19:09:00 -0400

    intro into rxjs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fromEvent, interval, timer } from 'rxjs';
 
 @Component({
     selector: 'about',
@@ -11,6 +12,27 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      const interval$ = timer(3000, 1000);
+
+      const sub = interval$.subscribe(val => {
+        console.log(`stream => `, val);
+      })
+
+      setTimeout(() => {
+        sub.unsubscribe();
+        console.log(`unsubscribed`)
+      }, 5000)
+      
+      const click$ = fromEvent(document, 'click');
+      click$.subscribe(
+        evt => console.log(evt),
+        err => console.log(err),
+        () => console.log(`completed`)
+                
+      
+      );
+      
+
   }
 
 }
